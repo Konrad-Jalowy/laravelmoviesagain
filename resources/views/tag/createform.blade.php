bb40aacf2e7747a8bda52580a8a71f59ed138653
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-20 09:53:01 +0200

    enter tag form

---
+++ resources/views/tag/createform.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+          <form action="{{route('roles.store')}}" method="POST">
+            @csrf
+            <label for="name">Tag name:</label>
+            <input type="text" name="name" id="name" placeholder="Enter tag">
+            <input type="submit" value="Add">
+          </form>
+        </div>
+    </div>
+</div>
+@endsection
