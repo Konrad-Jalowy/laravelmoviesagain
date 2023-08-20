cf1bf890da4a475d005d120feac6b0fb88fcd0e7
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-20 11:47:19 +0200

    sth

---
+++ database/migrations/2023_08_20_093734_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->date('date_of_publishing');
+            $table->text('review');
+            $table->integer('grade');
+            $table->bigInteger('movie_length');
+            $table->foreign('director_id')->references('id')->on('directors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+};
