4f8d6db609f98a08d47da07a18603db1046a0e44
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-21 02:01:36 +0200

    relations in models

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    public function director()
+    {
+        return $this->belongsTo(Movie::class);
+    }
+
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
 }
