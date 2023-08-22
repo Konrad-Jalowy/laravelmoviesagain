6e9c658087224ce145590d548d069d030d03cb01
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-22 06:44:51 +0200

    link from director to movie

--- resources/views/director/showone.blade.php
+++ resources/views/director/showone.blade.php
@@ -6,7 +6,8 @@
     <p>Bio: {{$director->bio}}</p>
     <ul>
     @forelse ($director->movies as $movie )
-       <li><strong>{{$movie->title}}</strong></li>
+       <li><a href="{{route('movies.show', $movie->id)}}"><strong>{{$movie->title}}</strong></a>
+        </li>
     @empty
         <li>No movies yet!</li>
     @endforelse
