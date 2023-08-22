36be72164c1b8b5cc9b4bff8ec544aa72501cff5
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-22 06:41:10 +0200

    yet another link

--- resources/views/category/showone.blade.php
+++ resources/views/category/showone.blade.php
@@ -5,7 +5,8 @@
     <p>Category: {{$category->name}}</p>
     <ul>
     @forelse ($category->movies as $movie )
-       <li><strong>{{$movie->title}}</strong></li>
+       <li><a href="{{route('movies.show', $movie->id)}}"><strong>{{$movie->title}}</strong></a>
+       </li>
     @empty
         <li>No movies yet!</li>
     @endforelse
