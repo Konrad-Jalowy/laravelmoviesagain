386e81ff16b7538ef36da7cea08d9608eca0bebe
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-23 08:25:03 +0200

    custom messages when nothing found using filters

--- resources/views/article/showall.blade.php
+++ resources/views/article/showall.blade.php
@@ -7,7 +7,18 @@
     @forelse ($articles as $article )
        <li><a href="{{route('articles.show', $article->id)}}"><strong>{{$article->title}}</strong></a></li>
     @empty
-        <li>No articles yet!</li>
+            @if(request()->get('filter') == null)
+            <li>No articles yet!</li>
+            @else
+                @switch(request()->get('filter'))
+                    @case('noanswer')
+                    <li>Currently no unanswered articles!</li>
+                        @break
+                    @default
+                    <li>No articles yet!</li>    
+                @endswitch
+            @endif
+            
     @endforelse
     </ul>
 </div>
