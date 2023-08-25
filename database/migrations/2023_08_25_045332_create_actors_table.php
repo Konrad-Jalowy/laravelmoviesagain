9c6cb72ddc8ba40e96ae524fb10e677d1cb9b73c
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-25 14:48:20 +0200

    actors migration

--- database/migrations/2023_08_25_045332_create_actors_table.php
+++ database/migrations/2023_08_25_045332_create_actors_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('actors', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
+            $table->date('date_of_birth');
+            $table->text('bio');
         });
     }
 
