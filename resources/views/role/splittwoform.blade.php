866c9af70d791d43033f06b032e81948ec10b850
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-26 09:01:35 +0200

    adding bootstrap to forms

--- resources/views/role/splittwoform.blade.php
+++ resources/views/role/splittwoform.blade.php
@@ -6,21 +6,21 @@
         <div class="col-md-8">
           <form action="{{route('splitRole')}}" method="POST">
             @csrf
-            <label for="user">User:</label>
-            <select name="user" id="user">
+            <label for="user" class="form-label">User:</label>
+            <select name="user" id="user" class="form-select">
                 @foreach ( $users as $user )
                     <option value="{{$user->id}}">{{$user->name}}</option>
                 @endforeach
             </select>
-            <br>
-            <label for="role">Role:</label>
-            <select name="role" id="role">
+
+            <label for="role" class="form-label">Role:</label>
+            <select name="role" id="role" class="form-select">
                 @foreach ( $roles as $role )
                     <option value="{{$role->id}}">{{$role->name}}</option>
                 @endforeach
             </select>
-            <br>
-            <input type="submit" value="Take away membership">
+     
+            <input type="submit" value="Take away membership" class="btn btn-primary mt-2">
           </form>
         </div>
     </div>
