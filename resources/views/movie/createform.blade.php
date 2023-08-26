9b7ce097e78598e5cf20f9dc2dc473fa9b5d6487
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-26 09:05:29 +0200

    adding bootstrap to forms

--- resources/views/movie/createform.blade.php
+++ resources/views/movie/createform.blade.php
@@ -6,43 +6,43 @@
         <div class="col-md-8">
           <form action="{{route('movies.store')}}" method="POST">
             @csrf
-            <label for="name">Movie title:</label>
-            <br>
-            <input type="text" name="title" id="title" placeholder="Enter title">
-            <br>
-            <label for="date_of_publishing">Date of publishing:</label>
-            <br>
-            <input type="date" name="date_of_publishing" id="date_of_publishing">
-            <br>
-            <label for="movie_length">Movie length:</label>
-            <br>
-            <input type="number" name="movie_length" id="movie_length" min="1">
-            <br>
-            <label for="grade">Movie grade:</label>
-            <br>
-            <input type="number" name="grade" id="grade" min="1" max="10">
-            <br>
-            <label for="review">Review:</label>
-            <br>
-            <textarea name="review" id="review" cols="30" rows="10"></textarea>
-            <br>
-            <label for="director">Director:</label>
-            <br>
-            <select name="director" id="director">
+            <label for="name" class="form-label">Movie title:</label>
+   
+            <input type="text" name="title" id="title" placeholder="Enter title" class="form-control">
+       
+            <label for="date_of_publishing" class="form-label">Date of publishing:</label>
+
+            <input type="date" name="date_of_publishing" id="date_of_publishing" class="form-control">
+        
+            <label for="movie_length" class="form-label">Movie length:</label>
+    
+            <input type="number" name="movie_length" id="movie_length" min="1" class="form-control">
+     
+            <label for="grade" class="form-label">Movie grade:</label>
+    
+            <input type="number" name="grade" id="grade" min="1" max="10" class="form-control">
+      
+            <label for="review" class="form-label">Review:</label>
+    
+            <textarea name="review" id="review" cols="30" rows="10" class="form-control"></textarea>
+       
+            <label for="director" class="form-label">Director:</label>
+         
+            <select name="director" id="director" class="form-select">
                 @foreach ($directors as $director )
                     <option value="{{$director->id}}">{{$director->name}}</option>
                 @endforeach
             </select>
-            <br>
-            <label for="category">Category:</label>
-            <br>
-            <select name="category" id="category">
+       
+            <label for="category" class="form-label">Category:</label>
+     
+            <select name="category" id="category" class="form-select">
                 @foreach ($categories as $category )
                     <option value="{{$category->id}}">{{$category->name}}</option>
                 @endforeach
             </select>
-            <br>
-            <input type="submit" value="Add movie">
+       
+            <input type="submit" value="Add movie" class="btn btn-primary mt-2">
           </form>
         </div>
     </div>
