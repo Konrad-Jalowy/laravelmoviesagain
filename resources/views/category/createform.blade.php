47cf7a325d3bf58ddace631b67d36dd9b33820ab
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-26 09:16:44 +0200

    bootstrap form controls

--- resources/views/category/createform.blade.php
+++ resources/views/category/createform.blade.php
@@ -6,9 +6,9 @@
         <div class="col-md-8">
           <form action="{{route('categories.store')}}" method="POST">
             @csrf
-            <label for="name">Category name:</label>
-            <input type="text" name="name" id="name" placeholder="Enter category">
-            <input type="submit" value="Add category">
+            <label for="name" class="form-label">Category name:</label>
+            <input type="text" name="name" id="name" placeholder="Enter category" class="form-control">
+            <input type="submit" value="Add category" class="btn btn-primary mt-2">
           </form>
         </div>
     </div>
