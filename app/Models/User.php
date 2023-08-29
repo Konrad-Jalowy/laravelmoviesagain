f27f5c7471642f9938bf672f714923006e1bf75c
Konrad-Jalowy <sitesystem@wp.pl>
2023-08-29 05:52:15 +0200

    relation between user and upload

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function uploads() {
+        return $this->hasMany(Upload::class);
+    }
+
     public function roles() {
         return $this->belongsToMany(Role::class);
     }
